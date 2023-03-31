09fbee334015e4478b45266a23b89f68fd6078b1
dsullivan21 <deveshsullivan21@gmail.com>
2023-03-31 08:35:57 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,13 +3,7 @@ import "firebase/auth";
 import "firebase/firestore";
 
 var firebaseConfig = {
-    apiKey: "AIzaSyCF18nCB1wXwHipUa92PUWLWyGEyN-fGEY",
-    authDomain: "minigolf-f2268.firebaseapp.com",
-    projectId: "minigolf-f2268",
-    storageBucket: "minigolf-f2268.appspot.com",
-    messagingSenderId: "181198278584",
-    appId: "1:181198278584:web:868239f74f27eae6c2c94d",
-    measurementId: "G-WYJ5YGF8TM"
+    
 };
 
 let app;
